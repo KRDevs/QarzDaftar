1b8b538a4c5599b70f90501d6d00fd98d456614e
Ziyodulla-Abdukarimov <ZiyodullaDeveloper@gmail.com>
2024-04-15 14:47:47 +0500

    ok

--- app/src/main/java/com/example/qarzdaftar/DebtFragment.java
+++ app/src/main/java/com/example/qarzdaftar/DebtFragment.java
@@ -1,42 +1,31 @@
 package com.example.qarzdaftar;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DebtFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+
 public class DebtFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private ListView listView;
+    private ListAdapter listAdapter;
+    private ArrayList<User> userArrayList;
 
+    // Required empty public constructor
     public DebtFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+    // Use this factory method to create a new instance of this fragment
     public static DebtFragment newInstance(String param1, String param2) {
         DebtFragment fragment = new DebtFragment();
         Bundle args = new Bundle();
@@ -55,10 +44,23 @@ public class DebtFragment extends Fragment {
         }
     }
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_debt, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_debt, container, false);
+        listView = view.findViewById(R.id.debt_list);
+
+        // Initialize userArrayList with your data
+        userArrayList = new ArrayList<>();
+        userArrayList.add(new User("Komron Raximov", "12000", "12.04.2024"));
+        userArrayList.add(new User("Og'abek Xusinov", "100000", "10.04.2024"));
+        userArrayList.add(new User("Ziyodulla Abdukarimov", "150000", "14.04.2024"));
+        userArrayList.add(new User("Elomon Jumanazarov", "10000000", "03.04.2024"));
+
+        // Initialize listAdapter and set it to the ListView
+        listAdapter = new ListAdapter(getActivity(), userArrayList);
+        listView.setAdapter(listAdapter);
+
+        return view;
     }
 }
